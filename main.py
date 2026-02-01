f03e55d61a629ac372dc6e828afd9b6cda4236a3
umarprogrammer19 <umarprogrammer19666@gmail.com>
2026-02-01 16:25:36 +0500

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request
 from src.ai import analyze_text
 from src.db import save_lead
+from src.logger import logger
 
 app = FastAPI()
 
@@ -15,5 +16,8 @@ async def reddit_webhook(request: Request):
     ai = analyze_text(text, "reddit")
 
     save_lead(text, url, ai)
+
+    logger.info(f"Received Webhook Data: {data}")
+    logger.info(f"AI Result: {ai}")
 
     return {"status": "received", "ai": ai}
